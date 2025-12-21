6409d7536e646f7dbc1eaa28a67fcf8db19acc08
Andrea Galimberti <andrea.galimberti05@gmail.com>
2025-12-21 18:46:45 +0100

    fix to prolog is_regex implementation

--- Prolog/nsfa.pl
+++ Prolog/nsfa.pl
@@ -1,45 +1,24 @@
-% -----------------------------------------------------------
-% 1. DEFINIZIONE DELLA WHITELIST (I tuoi funtori permessi)
-% Qui elenchi SOLO i funtori che accetti come nodi validi.
-% -----------------------------------------------------------
-funtore_permesso(seq).   % Sequenza
-funtore_permesso(star).  % Stella di Kleene
-funtore_permesso(or).    % Scelta alternativa
-funtore_permesso(plus).  % Esempio: uno o più
-% funtore_permesso(c).   % Se vuoi usare 'c' come nel tuo esempio precedente
+is_regex(RE):-
+    atomic(RE).
 
-% -----------------------------------------------------------
-% 2. CASO LISTA VUOTA (Stop)
-% -----------------------------------------------------------
-is_regex([]) :- !.
+is_regex(RE):-
+    compound(RE),
+    RE =.. [Fun | Args],
+    ( (member(Fun, [c, a]), length(Args, N), N >= 2)
+    ; (member(Fun,[z,o]), length(Args,1))
+    ),
+    !,
+    is_regex_args(Args).
 
-% -----------------------------------------------------------
-% 3. CASO LISTA NON VUOTA (Iterazione argomenti)
-% -----------------------------------------------------------
-is_regex([Testa | Coda]) :- 
-    !, 
-    is_regex(Testa), 
-    is_regex(Coda).
+is_regex(RE):-
+    compound(RE),
+    RE =.. [Functor | _],
+    \+ member(Functor, [c,a,z,o]).
 
-% -----------------------------------------------------------
-% 4. CASO ATOMO/NUMERO (Foglia)
-% -----------------------------------------------------------
-is_regex(Termine) :-
-    atomic(Termine), 
-    !.
+is_regex_args([]).
+is_regex_args([H | T]):-
+    is_regex(H),
+    is_regex_args(T).
 
-% -----------------------------------------------------------
-% 5. CASO COMPOSTO (Nodo dell'albero) - MODIFICATO
-% -----------------------------------------------------------
-is_regex(Termine) :-
-    compound(Termine),
-    
-    % A. Controllo validità: Il funtore DEVE esistere nella whitelist
-    functor(Termine, Funtore, _),
-    funtore_permesso(Funtore),  % <--- QUI: Verifica positiva (senza \+)
-    
-    % B. Estrazione Argomenti
-    Termine =.. [_ | Argomenti], 
-    
-    % C. Ricorsione sui figli
-    is_regex(Argomenti).
+
+
