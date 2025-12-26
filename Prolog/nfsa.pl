9d34eec7f392b163c91b50eb2cfd532bdef4124c
Andrea Galimberti <andrea.galimberti05@gmail.com>
2025-12-26 19:54:24 +0100

    rename file

---
+++ Prolog/nfsa.pl
@@ -0,0 +1,37 @@
+%%% -*- Mode: Prolog -*-
+
+%%% nsfa.pl starts here
+
+%!   is_regex/1
+%
+%   True when RE is a regular expression.
+%   
+
+is_regex(RE):-
+    atomic(RE).
+
+is_regex([]).
+is_regex([H | T]):-
+    !,
+    is_regex(H),
+    is_regex(T).
+
+is_regex(RE):-
+    compound(RE),
+    RE =.. [Fun | Args],
+    ( (member(Fun, [c, a]), length(Args, N), N >= 2)
+    ; (member(Fun,[z,o]), length(Args,1))
+    ),
+    !,
+    is_regex(Args).
+
+is_regex(RE):-
+    compound(RE),
+    RE =.. [Functor | _],
+    \+ member(Functor, [c,a,z,o]).
+
+
+%%% nsfa.pl ends here
+
+
+
